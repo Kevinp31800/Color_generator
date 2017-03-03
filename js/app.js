d10dfaef5343de479f6c8adfffaa02b692b74336
kevin <kevinpuyol@live.fr>
2017-03-03 11:25:45 +0100

    arrangement input

--- js/app.js
+++ js/app.js
@@ -9,41 +9,52 @@ $(document).ready (function () {
   var Green = 100;
   var Blue = 100;
 
-
-  $('#r1').on("change mousemove",function(){
-    red = $(this).val();
-    $('#body').css({'background-color': 'rgb('+ red + ', ' +  green + ', ' + blue + ')'});
-    console.log($('#r1').val(), $('#g1').val(), $('#b1').val());
+  $('#r1').click(function(){
+    $(this).on("change mousemove",function(){
+      red = $(this).val();
+      $('#body').css({'background-color': 'rgb('+ red + ', ' +  green + ', ' + blue + ')'});
+      console.log($('#r1').val(), $('#g1').val(), $('#b1').val());
+    })
   });
-  $('#g1').on("change mousemove",function(){
-    green = $(this).val();
-    $('#body').css({'background-color': 'rgb('+ red + ', ' +  green + ', ' + blue + ')'});
-    console.log($('#r1').val(), $('#g1').val(), $('#b1').val());
+  $('#g1').click(function(){
+    $(this).on("change mousemove",function(){
+      green = $(this).val();
+      $('#body').css({'background-color': 'rgb('+ red + ', ' +  green + ', ' + blue + ')'});
+      console.log($('#r1').val(), $('#g1').val(), $('#b1').val());
+    })
   });
-  $('#b1').on("change mousemove",function(){
-   blue = $(this).val();
-   $('#body').css({'background-color': 'rgb('+ red + ', ' +  green + ', ' + blue + ')'});
-   console.log($('#r1').val(), $('#g1').val(), $('#b1').val());
- });
+  $('#b1').click(function(){
+    $(this).on("change mousemove",function(){
+     blue = $(this).val();
+     $('#body').css({'background-color': 'rgb('+ red + ', ' +  green + ', ' + blue + ')'});
+     console.log($('#r1').val(), $('#g1').val(), $('#b1').val());
+   })
+  });
 
   
-  $('#r2').on("change mousemove",function(){
-    Red = $(this).val();
-    $('#body').css({'background': "linear-gradient(to right, rgb(" + Red + "," + Green + "," + Blue + "),rgb(" + red + "," + green + "," + blue + ")"}); 
-    console.log($('#r2').val());
-  })
+  $('#r2').click(function(){
+    $(this).on("change mousemove",function(){
+      Red = $(this).val();
+      $('#body').css({'background': "linear-gradient(to right, rgb(" + Red + "," + Green + "," + Blue + "),rgb(" + red + "," + green + "," + blue + ")"}); 
+      console.log($('#r2').val());
+    })
+  });
 
-  $('#g2').on("change mousemove",function(){
-    Green = $(this).val();
-    $('#body').css({'background': "linear-gradient(to right, rgb(" + Red + "," + Green + "," + Blue + "),rgb(" + red + "," + green + "," + blue + ")"}); 
-    console.log($('#g2').val());
-  })
-  $('#b2').on("change mousemove",function(){
-    Blue = $(this).val();
-    $('#body').css({'background': "linear-gradient(to right, rgb(" + Red + "," + Green + "," + Blue + "),rgb(" + red + "," + green + "," + blue + ")"}); 
-    console.log($('#r2').val()); 
-    console.log($('#b2').val());
-  })
+  $('#g2').click(function(){
+    $(this).on("change mousemove",function(){
+      Green = $(this).val();
+      $('#body').css({'background': "linear-gradient(to right, rgb(" + Red + "," + Green + "," + Blue + "),rgb(" + red + "," + green + "," + blue + ")"}); 
+      console.log($('#g2').val());
+    })
+  });
+  $('#b2').click(function(){
+    $(this).on("change mousemove",function(){
+      Blue = $(this).val();
+      $('#body').css({'background': "linear-gradient(to right, rgb(" + Red + "," + Green + "," + Blue + "),rgb(" + red + "," + green + "," + blue + ")"}); 
+      console.log($('#r2').val()); 
+      console.log($('#b2').val());
+    })
+  });
 
 
 
